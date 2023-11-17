035aee3a0ef0102e724d77e1f198996754c0199f
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-16 21:49:35 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
+from flask import Flask, send_from_directory
+from werkzeug.utils import secure_filename
+import os
 
-# Specify the directory containing your ROMs
-ROMS_DIR = "/media/httpanimations/Roms"
+app = Flask(__name__)
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=ROMS_DIR, **kwargs)
+@app.route('/')
+def index():
+    return 'Hello, this is the ROM server!'
 
-# Create an instance of the server
-with TCPServer(("0.0.0.0", 80), MyHandler) as httpd:
-    print("Server is running on port 80...")
-    # Start the server
-    httpd.serve_forever()
+@app.route('/list')
+def list_folders():
+    roms_dir = '/media/httpanimations/Roms'
+    folders = [f for f in os.listdir(roms_dir) if os.path.isdir(os.path.join(roms_dir, f))]
+    return ','.join(folders)
+
+@app.route('/list/<folder>')
+def list_files(folder):
+    roms_dir = '/media/httpanimations/Roms'
+    folder_path = os.path.join(roms_dir, folder)
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    return ','.join(files)
+
+@app.route('/download/<folder>/<filename>')
+def download_file(folder, filename):
+    roms_dir = '/media/httpanimations/Roms'
+    folder_path = os.path.join(roms_dir, folder)
+    return send_from_directory(folder_path, filename)
+
+if __name__ == '__main__':
+    # Get the host IP address and port
+    host = '0.0.0.0'
+    port = 80
+
+    print(f"Server running at http://{host}:{port}/")
+    app.run(debug=True, host=host, port=port)
